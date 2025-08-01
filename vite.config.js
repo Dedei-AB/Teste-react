15195c11c9f7d6ffa3d8bae29738b88199beb948
Dedei-AB <andreiabners@gmail.com>
2025-08-01 16:09:12 -0300

    atualizada boa

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Teste-react/",
   server: {
     proxy: {
-      '/api': {
-        target: 'https://api.exchangerate.host',
+      "/api": {
+        target: "https://api.exchangerate.host",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
-
+});
